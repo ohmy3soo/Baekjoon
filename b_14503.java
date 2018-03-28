aa04f0ae80aa21a0c5fc96e0392a11ac441aaefe
ahn <ohmy3soo@nate.com>
2018-03-28 20:43:23 +0900

    .

--- b_14503.java
+++ b_14503.java
@@ -1,3 +1,5 @@
+// DFS로 풀어보기
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,37 +13,37 @@ public class b_14503 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String input = in.readLine();
-		
+
 		StringTokenizer st = new StringTokenizer(input);
-		
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+
 		map = new int[N][M];
 		input = in.readLine();
 		st = new StringTokenizer(input);
-		
+
 		posX = Integer.parseInt(st.nextToken());
 		posY = Integer.parseInt(st.nextToken());
 		dir = Integer.parseInt(st.nextToken());
-		
+
 		for(int i=0; i<N; i++) {
 			input = in.readLine();
 			st = new StringTokenizer(input);
 			for(int j=0; j<M; j++)
 				map[i][j] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		map[posX][posY] = -1;
 		int count = 1;
-		
+
 		while(true) {
 			boolean move = false;
 			for(int i=0; i<4; i++) {
 				dir = (dir+3)%4;
 				int nextX = posX + next[dir][0];
 				int nextY = posY + next[dir][1];
-				
+
 				if(map[nextX][nextY] == 0){
 					posX = nextX;
 					posY = nextY;
@@ -53,17 +55,16 @@ public class b_14503 {
 			}
 			if(move == true)
 				continue;
-			
+
 			posX += next[(dir+2)%4][0];
 			posY += next[(dir+2)%4][1];
-			
+
 			if(map[posX][posY] == 1) {
 				System.out.println(count);
 				return;
 			}
-			
-			
+
+
 		}
 	}
 }
-		
