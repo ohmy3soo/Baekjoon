beb25b56f52bfe6b712252c7153db9386202f035
ahn <ohmy3soo@nate.com>
2018-04-16 11:12:18 +0900

    N 찍기

--- b_2741.java
+++ b_2741.java
@@ -5,7 +5,17 @@ public class b_2741 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=N; i++) {
+			sb.append(i + "\n");
+		}
+		System.out.print(sb);
+		/*
 		for(int i=1; i<=N; i++)
 			System.out.println(i);
+		*/
+
+		// StringBuffer -> StringBuilder
+
 	}
 }
