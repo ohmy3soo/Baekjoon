59b62d56269233585f70e02a7f79afb178a90591
ahn <ohmy3soo@nate.com>
2018-07-28 22:55:31 +0900

    회사에 있는 사람

--- b_7785.java
+++ b_7785.java
@@ -4,19 +4,28 @@ import java.io.*;
 public class b_7785 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
 		StringTokenizer st;
-		ArrayList<String> a = new ArrayList<>();
+
+		HashMap<String, Boolean> hm = new HashMap<>();
 		int n = Integer.parseInt(in.readLine());
 		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(in.readLine());
 			String name = st.nextToken();
 			String state = st.nextToken();
 			if(state.equals("enter"))
-				a.add(name);
+				hm.put(name, true);
 			else if(state.equals("leave"))
-				a.remove(name);
+				hm.put(name, false);
 		}
 
+		ArrayList<String> a = new ArrayList<>();
+		Iterator<String> it = hm.keySet().iterator();
+		while(it.hasNext()) {
+			String next = it.next();
+			if(hm.get(next)) a.add(next);
+		}
 		Collections.sort(a, new Comparator<String>() {
 			@Override
 			public int compare(String o1, String o2) {
@@ -26,6 +35,7 @@ public class b_7785 {
 		});
 
 		for(String name : a)
-			System.out.println(name);
+			sb.append(name + "\n");
+		System.out.println(sb);
 	}
 }
